5681a2b8276c8809e84fb2a065182c18278cb642
Marc A. Ziegert <comonad@gmail.com>
2020-08-13 00:27:06 +0200

    diagrams test.ps

--- Main.hs
+++ Main.hs
@@ -1,10 +1,27 @@
-module Main where
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE TypeFamilies              #-}
 
 import qualified Wappo
 
+import Diagrams.Prelude
+--import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Backend.Postscript.CmdLine as Backend
 
 
+
+
+-- Backend.pagesMain :: [QDiagram Postscript V2 Double Any] -> IO ()
+-- type Diagram b = QDiagram b (V b) (N b) Any
+
+
+test :: [Diagram B]
+test = [square 1 # fc aqua `atop` circle 1]
 
 main :: IO ()
-main = Wappo.main
+main = do
+  Backend.pagesMain (test::[Diagram B])
+  --Wappo.main
+
+
 
